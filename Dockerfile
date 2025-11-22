67703ddffcdafe49374dfa179cfddb1013b06936
Ananya-0507 <vvce23cse0024@vvce.ac.in>
2025-11-22 17:37:57 +0530

    Copy static frontend files in Docker image

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node:18-alpine
 WORKDIR /app
 COPY app/package.json ./
 RUN npm install --production
-COPY app/ ./
+COPY app/server.js ./
+COPY app/test.js ./
+COPY app/public ./public
 EXPOSE 3000
-CMD ["npm", "start"]
+CMD ["node", "start"]
--- app/server.js
+++ app/server.js
@@ -12,7 +12,7 @@ app.get('/todos', (req, res) => res.json(todos));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.listen(3000, () => {
   console.log("Todo app running on port 3000");
 });
